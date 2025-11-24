93fd1a990ca9d8b08858278e3748925f29a01737
Lukáš Kníže <82168954+LukasKnize@users.noreply.github.com>
2025-11-24 20:23:49 +0100

    Add files via upload

---
+++ vyhledavani/search.js
@@ -0,0 +1,25 @@
+const search = (params) => {
+  let thisPage = new URL(location.origin + "/vyhledavani/");
+  thisPage.searchParams.append("search", params);
+  window.location.href = thisPage;
+};
+
+const mainSearchBar = document.querySelector("#mainSearchBar");
+
+const mainSearch = () => {
+  search(mainSearchBar.value);
+};
+
+if (mainSearchBar) {
+  const params = new URLSearchParams(document.location.search);
+  const searchParam = params.get("search");
+  if (searchParam) {
+    mainSearchBar.value = searchParam;
+  }
+}
+
+const mobileSearchBar = document.querySelector("#mobileSearchBar");
+
+const mobileSearch = () => {
+  search(mobileSearchBar.value);
+};
